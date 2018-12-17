d5c95935215988e89c3b7ecb6a6215b81d855ada
Kara Bayse <karabayse@gmail.com>
2018-12-17 17:29:15 -0600

    added body

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import Header from '../Header/Header';
+import Body from '../Body/Body';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Body />
       </div>
     );  // end JSX  ->  what gets rendered to DOM in this component, and is returned
   }  //  end render  ->  necessary method to render onto DOM
---
+++ my-app/src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return(
+      <div>
+        <p>Body</p>
+      </div> // end JSX
+    ); // end return
+  } // end render method
+} // end Component
+
+export default Body;
