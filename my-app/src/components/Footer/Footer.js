e22ee99206746e525821447737f481231adada06
Kara Bayse <karabayse@gmail.com>
2018-12-17 17:38:49 -0600

    added footer css

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      <div>
+      <div className="App-footer">
         <h2>Footer</h2>
       </div> // end containing div
     ); // end JSX
