0484e664d4159a791a4a421626342db028056a5c
Kara Bayse <karabayse@gmail.com>
2018-12-17 17:35:16 -0600

    added footer

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import Header from '../Header/Header';
 import Body from '../Body/Body';
+import Footer from '../Footer/Footer'
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Body />
+        <Footer />
       </div>
     );  // end JSX  ->  what gets rendered to DOM in this component, and is returned
   }  //  end render  ->  necessary method to render onto DOM
