760cb96799b7b036738cb116af0fe35229cd38fc
Kara Bayse <karabayse@gmail.com>
2018-12-19 07:28:31 -0600

    fixed username id

--- my-app/src/components/Body/Body.js
+++ my-app/src/components/Body/Body.js
@@ -12,7 +12,8 @@ class Body extends Component {
   // addUsername arrow function expression which is a method
   // using the function expression auto-binds this method to this class
   addUsername = () => {
-    console.log('in addUsername function');
+    const usernameIn = document.getElementById('usernameIn').value;
+    console.log('in addUsername function:', usernameIn);
   } // end addUsername
 
   // render method
@@ -20,13 +21,14 @@ class Body extends Component {
     return(
       <div>
         <h1>Add Username</h1>
-        <input type="text" placeholder="username"></input>
+        <input type="text" placeholder="username" id="usernameIn"></input>
         <button onClick={ this.addUsername }>Add Username</button>
         <ul>
           <li>un00</li>
           <li>un01</li>
           <li>un02</li>
           <li>un03</li>
+          <li>{ this.state.testTest }</li>
         </ul>
       </div> // end containing div
     ); // end JSX
