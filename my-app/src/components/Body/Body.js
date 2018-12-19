d70db2aafe5bdeca354bc44149450006594f0ad0
Kara Bayse <karabayse@gmail.com>
2018-12-18 18:49:46 -0600

    added input, button, unordered list to Body

--- my-app/src/components/Body/Body.js
+++ my-app/src/components/Body/Body.js
@@ -4,7 +4,15 @@ class Body extends Component {
   render() {
     return(
       <div>
-        <p>Body</p>
+        <h1>Add Username</h1>
+        <input type="text" placeholder="username"></input>
+        <button>Add Username</button>
+        <ul>
+          <li>un00</li>
+          <li>un01</li>
+          <li>un02</li>
+          <li>un03</li>
+        </ul>
       </div> // end containing div
     ); // end JSX
   } // end render method
